72f9c22fd19883e18965aba21bbe66909bfcd8ff
Germann Arlington <germann_arlington@yahoo.co.uk>
2016-08-25 13:59:43 +0100

    V1.0.1 commit

--- src/main/java/org/trading/exchange/interfaces/Exchanged.java
+++ src/main/java/org/trading/exchange/interfaces/Exchanged.java
@@ -1,15 +1,7 @@
 package org.trading.exchange.interfaces;
 
-import org.trading.exchange.publicInterfaces.Exchangeable;
-
-import java.io.Serializable;
-import java.util.Collection;
-
 /**
  * Created by GArlington.
  */
-public interface Exchanged extends Serializable {
-	Exchangeable getExchangeable();
-
-	Collection<Exchangeable> getMatchedExchangeables();
+public interface Exchanged extends org.trading.exchange.publicInterfaces.Exchanged {
 }
